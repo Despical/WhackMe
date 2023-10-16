cf774081365b7c860dc6d671e0d6f4f748636573
Berke Akçen <berkeakcen@gmail.com>
2023-10-16 11:18:08 +0300

    Removed public usage of arena config

--- src/main/java/me/despical/whackme/handlers/setup/components/SetupComponent.java
+++ src/main/java/me/despical/whackme/handlers/setup/components/SetupComponent.java
@@ -6,7 +6,6 @@ import me.despical.inventoryframework.pane.StaticPane;
 import me.despical.whackme.WhackMe;
 import me.despical.whackme.handlers.ChatManager;
 import me.despical.whackme.handlers.setup.SetupInventory;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -18,11 +17,11 @@ public interface SetupComponent {
 
 	WhackMe plugin = JavaPlugin.getPlugin(WhackMe.class);
 	ChatManager chatManager = plugin.getChatManager();
-	FileConfiguration config = ConfigUtils.getConfig(plugin, "arenas");
 
 	void injectComponents(SetupInventory setupInventory, StaticPane pane);
 
 	default String isOptionDoneBool(String path) {
+		var config = ConfigUtils.getConfig(plugin, "arenas");
 		return config.isSet(path) ? LocationSerializer.isDefaultLocation(config.getString(path)) ? "&c&l✘ Not Completed" : "&a&l✔ Completed" : "&c&l✘ Not Completed";
 	}
 }
