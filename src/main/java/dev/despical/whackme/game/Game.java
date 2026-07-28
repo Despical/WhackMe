479bbdb76aef4e694300cc41181e43f7f89eef6d
Berke Akçen <berkeakcen@gmail.com>
2026-07-28 14:10:31 +0300

    Use configured duration in start message

--- src/main/java/dev/despical/whackme/game/Game.java
+++ src/main/java/dev/despical/whackme/game/Game.java
@@ -10,8 +10,10 @@ import dev.despical.whackme.game.states.GameStateHandler;
 import dev.despical.whackme.game.states.InGameState;
 import dev.despical.whackme.game.states.RestartingState;
 import dev.despical.whackme.game.states.WaitingState;
+import dev.despical.whackme.option.IntOption;
 import dev.despical.whackme.scoreboard.ScoreboardManager;
 import dev.despical.whackme.user.User;
+import dev.despical.whackme.util.Var;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -83,7 +85,7 @@ public final class Game extends BukkitRunnable {
 
         this.user = user;
         states.get(GameState.WAITING).join(user);
-        user.sendMessage(messagePath);
+        user.sendMessage(messagePath, Var.of("%gameplay_time%", IntOption.GAMEPLAY_TIME.value()));
         if (setState(GameState.IN_GAME)) {
             return true;
         }
