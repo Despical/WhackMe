cb90fba9e0d3784144342c55caa8830e175a7c67
Berke Akçen <berkeakcen@gmail.com>
2026-07-31 16:39:14 +0300

    Support static and generated arena defaults

--- src/main/java/dev/despical/whackme/arena/options/ArenaOption.java
+++ src/main/java/dev/despical/whackme/arena/options/ArenaOption.java
@@ -2,22 +2,36 @@ package dev.despical.whackme.arena.options;
 
 import lombok.Getter;
 
+import java.util.function.Supplier;
+
 /**
  * @author Despical
  * <p>
  * Created at 12.12.2025
  */
-@Getter
 public abstract class ArenaOption<T> {
 
-    private final String key;
+    private final @Getter String key;
+    private final @Getter Class<T> type;
     private final T defaultValue;
-    private final Class<T> type;
+    private final Supplier<? extends T> defaultSupplier;
 
-    public ArenaOption(String key, T defaultValue, Class<T> type) {
+    public ArenaOption(String key, Class<T> type, T defaultValue) {
         this.key = key;
         this.defaultValue = defaultValue;
+        this.defaultSupplier = null;
         this.type = type;
+    }
+
+    public ArenaOption(String key, Class<T> type, Supplier<? extends T> defaultSupplier) {
+        this.key = key;
+        this.defaultValue = null;
+        this.defaultSupplier = defaultSupplier;
+        this.type = type;
+    }
+
+    public T getDefaultValue() {
+        return defaultSupplier == null ? defaultValue : defaultSupplier.get();
     }
 
     public Object serialize(T value) {
@@ -33,7 +47,7 @@ public abstract class ArenaOption<T> {
             return parse(String.valueOf(value));
         } catch (Exception exception) {
             exception.printStackTrace();
-            return defaultValue;
+            return getDefaultValue();
         }
     }
 
