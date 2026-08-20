1800d84f33d6bf530380741429aa01d183985cde
Berke Akçen <berkeakcen@gmail.com>
2026-08-20 16:55:14 +0300

    Better debug component command

--- src/main/java/dev/despical/whackme/command/DebugCommands.java
+++ src/main/java/dev/despical/whackme/command/DebugCommands.java
@@ -1,9 +1,11 @@
 package dev.despical.whackme.command;
 
-import dev.despical.commandframework.CommandArguments;
 import dev.despical.commandframework.annotations.Command;
+import dev.despical.commandframework.annotations.Option;
 import dev.despical.commandframework.debug.Debug;
 import net.kyori.adventure.text.Component;
+
+import java.util.List;
 
 /**
  * @author Despical
@@ -17,11 +19,32 @@ public final class DebugCommands extends CommandCategory {
         name = "whackme.debug.component",
         aliases = "wm.debug.component",
         permission = "whackme.debug.component",
-        usage = "/%label% debug component <message>",
-        min = 1,
+        usage = "/%label% debug component [message] [--path=message.path]",
         senderType = Command.SenderType.PLAYER
     )
+    @Option("path")
     public void debugComponentCommand(Arguments arguments) {
+        List<String> pathOption = arguments.getOption("path");
+        String path = pathOption == null || pathOption.isEmpty() ? null : pathOption.getFirst();
+
+        if (path != null && !path.isBlank()) {
+            List<String> messages = chatManager.getStringList(path);
+            if (!messages.isEmpty()) {
+                messages.stream()
+                    .map(chatManager::parseMessage)
+                    .forEach(arguments::sendMessage);
+                return;
+            }
+
+            arguments.sendConfiguredMessage(path);
+            return;
+        }
+
+        if (arguments.isArgumentsEmpty()) {
+            arguments.sendRawMessage("<#FF5252>✖ <#BDBDBD>Provide a MiniMessage string or use <#FFCA28>--path=<message.path><#BDBDBD>.");
+            return;
+        }
+
         Component component = chatManager.parseMessage(arguments.concatArguments());
         arguments.sendMessage(component);
     }
